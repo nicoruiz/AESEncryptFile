f6dc30e5108237fcfe478f4888d8bb981cf34442
Miguel Nicolás Ruiz <nicoruiz182@gmail.com>
2019-12-01 18:41:25 -0300

    Initial commit

---
+++ LaboCrypto/LaboCrypto/AesEncryptor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace LaboCrypto
+{
+    public class AesEncryptor
+    {
+        public static byte[] Encrypt(string plainText, byte[] Key, byte[] IV)
+        {
+            byte[] encrypted;
+
+            using (AesManaged aes = new AesManaged())
+            {
+                //Create encryptor
+                ICryptoTransform encryptor = aes.CreateEncryptor(Key, IV);
+                //Create memory stream
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter sw = new StreamWriter(cs))
+                            sw.Write(plainText);
+                            encrypted = ms.ToArray();
+                    }
+                }
+            }
+            return encrypted;
+        }
+
+        public static string Decrypt(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            string plainText = String.Empty;
+
+            using (AesManaged aes = new AesManaged())
+            {
+                //Create decryptor
+                ICryptoTransform decryptor = aes.CreateDecryptor(Key, IV);
+                //Create memory stream
+                using (MemoryStream ms = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader reader = new StreamReader(cs))
+                            plainText = reader.ReadToEnd();
+                    }
+                }
+            }
+            return plainText;
+        }
+    }
+}
---
+++ LaboCrypto/LaboCrypto/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace LaboCrypto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = Path.GetRelativePath("@LaboCrypto", "..\\..\\TestFolder\\test_file.txt");
+            Console.WriteLine($"Reading file: {filePath}");
+            string fileText = System.IO.File.ReadAllText(filePath);
+            EncryptAesManaged(fileText);
+            Console.ReadLine();
+        }
+
+        static void EncryptAesManaged(string rawString)
+        {
+            try
+            {
+                using (AesManaged aes = new AesManaged())
+                {
+                    //Encrypt string
+                    byte[] encrypted = AesEncryptor.Encrypt(rawString, aes.Key, aes.IV);
+                    //Write encrypted file
+                    string encryptedText = System.Text.Encoding.UTF8.GetString(encrypted);
+                    Console.WriteLine("Writing encrypted file...");
+                    System.IO.File.WriteAllText(Path.GetRelativePath("@LaboCrypto", "..\\..\\TestFolder\\encrypted_file.txt"), encryptedText);
+                    //Write decrypted file
+                    Console.WriteLine("Writing decrypted file...");
+                    string decrypted = AesEncryptor.Decrypt(encrypted, aes.Key, aes.IV);
+                    System.IO.File.WriteAllText(Path.GetRelativePath("@LaboCrypto", "..\\..\\TestFolder\\decrypted_file.txt"), decrypted);
+                    Console.WriteLine("Done!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
